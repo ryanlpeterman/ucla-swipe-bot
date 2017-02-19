37d6a69e18f64bd83f39f54163ccf10dd135155f
Ryan Peterman <RyanLPeterman@gmail.com>
2017-02-18 22:24:06 -0800

    find postback

--- app.py
+++ app.py
@@ -32,20 +32,18 @@ def webhook():
 
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
-
+                log("Received Event!")
+                sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                 if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
                     # TODO: temporarily starts data request save on begin msg send
                     #       change to on "get started" button press instead later
-                    if message_text == "begin":
-                        send_message(sender_id, "Begin flow here")
+                    if message_text.lower() == "begin":
                         is_buyer(sender_id)
                     else:
-                        send_message(sender_id, "got it, thanks!")
+                        send_message(sender_id, "Got a message!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -87,11 +85,11 @@ def is_buyer(recipient_id):
                                 {
                                     "type": "postback",
                                     "title": "Buyer",
-                                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                    "payload": "buyer"
                                 },{
                                     "type": "postback",
                                     "title": "Seller",
-                                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                                    "payload": "seller"
                                 }
                             ]
                         }
