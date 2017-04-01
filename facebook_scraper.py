b417bef37c14d5ee8a76dd1ce2ddac8086c2b775
Nathan Zhang <natz@Nathans-MacBook-Pro-1725.local>
2017-04-01 15:14:39 -0700

    Basic working scraper. Will run once every 10 seconds and take posts from the last 2 hours.

--- facebook_scraper.py
+++ facebook_scraper.py
@@ -1,2 +1,47 @@
+import urllib2
+import json
+import datetime
+import csv
+import time
+
 def test_func():
-    print("Hello")
+    # Need to acquire a more lasting access token
+    access_token = "EAACEdEose0cBAGAPj0iEXkIp9HlyHEZA9FVcwodX9tgvuIIud6fv2xmp2yHAMATZCSTM3xZBaeoWfWmS5qi4C78CZCy7sYc1YEorBZCqS9FRqWuvihunvMoOMXg0PB2PBZAr53neUJOMrH0ob2Q4IcrTaD0OzOUZBITVYhodV1txYL4LZCOeEsb4ls5MoJDD1aMZD"
+    group_id = '478176852260001'
+    scrapeFacebookGroupFeedData(group_id, access_token)
+
+def request_until_succeed(url):
+    req = urllib2.Request(url)
+    success = False
+    while not success:
+        try:
+            response = urllib2.urlopen(req)
+            if response.getcode() == 200:
+                success = True
+        except Exception, e:
+            print e
+            time.sleep(5)
+
+            print "Error for URL %s: %s" % (url, datetime.datetime.now())
+
+    return response.read()
+
+def scrapeFacebookGroupFeedData(group_id, access_token):
+
+    scrape_interval = 7200 # Change this to adjust scrape time interval
+    since_time = str(int(time.time() - scrape_interval))
+    until_time = str(int(time.time()))
+    # construct the URL string
+    base = "https://graph.facebook.com/v2.3"
+    node = "/" + group_id + "/feed"
+    # Add or subtract from parameters to customize return data
+    parameters = "/?fields=created_time,from,message&since=%s&until=%s&access_token=%s" % (since_time, until_time, access_token)
+    url = base + node + parameters
+
+    # retrieve data
+    data = json.loads(request_until_succeed(url))
+
+    print json.dumps(data, indent=4, sort_keys=True)
+
+if __name__ == '__main__':
+    scrapeFacebookGroupFeedData(group_id, access_token)
