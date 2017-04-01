a4f0fe8d4ac52133faad0b90bd85ac16183a8242
Michael <Michael>
2017-04-01 14:45:28 -0700

    Minor changes to scraper

--- facebook_scraper.py
+++ facebook_scraper.py
@@ -4,21 +4,19 @@ import datetime
 import csv
 import time
 
+# Need to acquire a more lasting access token
 access_token = "EAACEdEose0cBAGAPj0iEXkIp9HlyHEZA9FVcwodX9tgvuIIud6fv2xmp2yHAMATZCSTM3xZBaeoWfWmS5qi4C78CZCy7sYc1YEorBZCqS9FRqWuvihunvMoOMXg0PB2PBZAr53neUJOMrH0ob2Q4IcrTaD0OzOUZBITVYhodV1txYL4LZCOeEsb4ls5MoJDD1aMZD"
 
 group_id = '478176852260001'
 
-scrape_interval = 3600
-since_time = int(time.time() - scrape_interval)
-until_time = int(time.time())
-since_time_str = str(since_time)
-until_time_str = str(until_time)
-
+scrape_interval = 3600 # Change this to adjust scrape time interval
+since_time = str(int(time.time() - scrape_interval))
+until_time = str(int(time.time()))
 
 def request_until_succeed(url):
     req = urllib2.Request(url)
     success = False
-    while success is False:
+    while not success:
         try:
             response = urllib2.urlopen(req)
             if response.getcode() == 200:
@@ -31,12 +29,13 @@ def request_until_succeed(url):
 
     return response.read()
 
-def testFacebookPageFeedData(group_id, access_token):
+def scrapeFacebookGroupFeedData(group_id, access_token):
 
     # construct the URL string
     base = "https://graph.facebook.com/v2.3"
-    node = "/" + group_id + "/feed" # changed
-    parameters = "/?fields=created_time,from,message&since=%s&until=%s&access_token=%s" % (since_time_str, until_time_str, access_token)
+    node = "/" + group_id + "/feed"
+    # Add or subtract from parameters to customize return data
+    parameters = "/?fields=created_time,from,message&since=%s&until=%s&access_token=%s" % (since_time, until_time, access_token)
     url = base + node + parameters
 
     # retrieve data
@@ -45,4 +44,4 @@ def testFacebookPageFeedData(group_id, access_token):
     print json.dumps(data, indent=4, sort_keys=True)
 
 if __name__ == '__main__':
-    testFacebookPageFeedData(group_id, access_token)
+    scrapeFacebookGroupFeedData(group_id, access_token)
